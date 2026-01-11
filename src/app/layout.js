e52ccc1f45ed2fcad6719d3d9a8e097659bb63af
ppxndd <natthapongp65@nu.ac.th>
2026-01-11 19:43:55 +0700

    change image promotion and qr contact

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,15 @@
 import './globals.css';
 
 export const metadata = {
-  title: "Coldfragrant"
+  title: "Coldfragrant",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
